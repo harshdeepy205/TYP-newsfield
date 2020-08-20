33bcfb05904661d97a3efeabe43770ab1bb09b72
harshdeepy205 <harshdeep205@outlook.com>
2020-08-20 14:02:32 +0530

    Second

---
+++ emp.js
@@ -0,0 +1,21 @@
+var arr1 = new Array();
+
+function addItem() {
+  var lists = document.getElementById("listItems");
+  var inputdata = document.getElementById("inputdata").value;
+  var li = document.createElement("li");
+
+  if (inputdata === "") {
+    alert("Enter the Text First");
+  } else {
+    if (arr1.includes(inputdata)) {
+      alert("News are already there");
+    } else {
+      li.setAttribute("id", inputdata);
+      li.appendChild(document.createTextNode(inputdata));
+      lists.appendChild(li);
+      arr1.push(inputdata);
+    }
+  }
+  document.getElementById("inputdata").value = "";
+}
