739fe6e463dee8dcc3ca1dec2dacfaad2a5afaa9
harshdeepy205 <harshdeep205@outlook.com>
2020-08-25 15:37:18 +0530

    Add Card

--- emp.js
+++ emp.js
@@ -56,3 +56,43 @@ function addNew() {
     element1 = document.getElementById("demo1").style.display = "none";
   }
 }
+
+function addNewCard(name, description, github_url, linkedin_url, instagram_url, twitter_url) {
+  console.log("addnewCrdWork", Date.now())
+  console.log(name, description, github_url, linkedin_url, instagram_url, twitter_url)
+
+
+  console.log(document.getElementById("divCards"))
+
+  document.getElementById("divCards").innerHTML += `<div class="register-box" id="${Date.now()}">
+  <center>
+    <h2>${name}</h2>
+  </center>
+  <div class="card-head">
+    <img src="img/harsh.jpg">
+  </div>
+  <div class="cdbody" style="margin-top: 10px;">
+    <center>
+      <h3>${description}</h3>
+    </center>
+    <a href="${github_url}" target="_blank" class="btn-img">
+      <i class="fa fa-github-square fa-2x " aria-hidden="true"></i>
+    </a>
+    <a href="${linkedin_url}" target="_blank" class="btn-img">
+
+      <i class="fa fa-linkedin-square fa-2x " aria-hidden="true"></i>
+    </a>
+    <a href="${instagram_url}" target="_blank" class="btn-img"><i class="fa fa-instagram fa-2x " aria-hidden="true"></i></a>
+    <a href="${twitter_url}"  target="_blank" class="btn-img"><i class="fa fa-twitter-square fa-2x " aria-hidden="true"></i></a>
+  </div>
+  <button onCLick ="removeCard(this.parentNode.id)" style="margin-top:14px; cursor:pointer;"> ❌ </button>
+</div>`
+}
+
+function removeCard(id_card) {
+
+  console.log("remoceCard ", id_card)
+
+  document.getElementById(id_card).style.display = 'none'
+
+}
